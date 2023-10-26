bde3b040a8414785a77dcc968d7a79664f5a5cd7
leticia.chijo <leticia.chijo@gmail.com>
2023-10-26 16:30:31 -0300

    Exercício

---
+++ README.md
@@ -0,0 +1,8 @@
+# typescript-06: Objetos tipados
+
+- Tipar usando tipos primitivos é bem simples, mas a tarefa começa a se tornar um pouco mais complexa quando objetos começam a aparecer.
+- Tipe o objeto e função a seguir.
+
+### Como rodar
+- Instale as dependências com o comando `npm i`.
+- Rode o comando `npm run dev` e veja os resultados no terminal.
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+const game = {
+    id: 1,
+    platform: {
+        id: 1,
+        name: "Playstation"
+    },
+    title: "The Last of Us",
+    publisher: "Naughty Dog",
+    launch: "2013-06-14" // pode ser um Date. É opcional (para jogos ainda não lançados).
+}
+
+const games = [game];
+
+function play(game){
+    // runs the game
+    console.log(game);
+}
